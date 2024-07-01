9f6559e4282f0f3a127844da3556011013c681e0
Titah Baudwin <baudwin12@gmail.com>
2024-07-01 02:33:07 +0100

    fixing deployment errors

--- backend/controllers/downloadsController.js
+++ backend/controllers/downloadsController.js
@@ -1,30 +1,30 @@
-require('dotenv').config()
-const earnings = require("../mockDB/dummyEarnings")
-const PDFDocument = require('pdfkit')
-const Papa = require('papaparse')
+// require('dotenv').config()
+// const earnings = require("../mockDB/dummyEarnings")
+// const PDFDocument = require('pdfkit')
+// const Papa = require('papaparse')
 
-module.exports = {
+// module.exports = {
 
-downloadAsPdf : async(req,res)=>{
+// downloadAsPdf : async(req,res)=>{
 
-    const doc = new PDFDocument();
-    doc.fontSize(12)
-    doc.text('Music Distribution Company - Earnings Report', {align: 'center', bold:true})
-    doc.moveDown();
-    earnings.forEach(earning=>{
-        doc.text(`Date : ${earning.date}     -    Earnings : ${earning.amount}`)
-        doc.moveDown();
-    })
+//     const doc = new PDFDocument();
+//     doc.fontSize(12)
+//     doc.text('Music Distribution Company - Earnings Report', {align: 'center', bold:true})
+//     doc.moveDown();
+//     earnings.forEach(earning=>{
+//         doc.text(`Date : ${earning.date}     -    Earnings : ${earning.amount}`)
+//         doc.moveDown();
+//     })
     
-    doc.pipe(res)
-    doc.end()
-    }, 
+//     doc.pipe(res)
+//     doc.end()
+//     }, 
 
 
-    downloadAsCsv : async(req,res)=>{
-        const csv = Papa.unparse(earnings)
-        res.send(csv)
-    }
+//     downloadAsCsv : async(req,res)=>{
+//         const csv = Papa.unparse(earnings)
+//         res.send(csv)
+//     }
 
-}
+// } 
 
